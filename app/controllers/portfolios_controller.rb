bd97c2f5a8033c3ae3822bc62a176a3a3d7d6b57
Karanveer Singh <singhkaranveer671@gmail.com>
2023-04-08 14:01:42 -0700

    feat(portfolio): edit and update

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,19 +1,31 @@
 class PortfoliosController < ApplicationController
-
+  before_action :find_portfolio, only: %i[show edit update]
   def index
     @portfolio_items = Portfolio.all
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def new
     @portfolio_item = Portfolio.new
   end
 
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: "portfolio item was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
   def create
-    @portfolio_item = Portfolio.new(blog_params)
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -23,8 +35,13 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  def blog_params
+
+  def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
+  def find_portfolio
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 end
