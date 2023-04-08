fe908c427f6693689129b9b39fcbc35d29fd9005
Karanveer Singh <singhkaranveer671@gmail.com>
2023-04-08 12:48:26 -0700

    feat(portfolio feat): portfolio passing data

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,27 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(blog_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: "portfolio item was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+  def blog_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
 end
