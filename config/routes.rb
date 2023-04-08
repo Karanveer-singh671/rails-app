7a2750c3f1fe371a91b0cefd18d094ecc15ddddd
Karanveer Singh <singhkaranveer671@gmail.com>
2023-04-08 14:50:09 -0700

    feat(route): custom route for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # when change route file restart rails server
+  # root to: means that by default it will go to pages home method
+  root to: 'pages#home'
+
 end
